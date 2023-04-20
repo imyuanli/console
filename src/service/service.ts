758eb39b213727915a43f10a84e191c194af9b11
yuanli <2865437316@qq.com>
2023-04-20 13:53:13 +0800

    refactor: 接口

--- src/service/service.ts
+++ src/service/service.ts
@@ -5,12 +5,6 @@ export async function get_project_list(payload?: any) {
     return request.get(BASE_URL + `blog/get_project_list/`, payload)
 }
 
-export async function insert_or_update_project(payload?: any) {
-    return request.post(BASE_URL + `console/insert_or_update_project/`, payload)
-}
-
-
-
 export async function get_article_list(payload?: any) {
     return request.get(BASE_URL + `blog/get_article_list/`, payload)
 }
@@ -19,7 +13,16 @@ export async function get_article(payload?: any) {
     return request.post(BASE_URL + `blog/get_article/`, payload)
 }
 
-export async function insert_or_update_blog(payload?: any) {
-    return request.post(BASE_URL + `console/insert_or_update_blog/`, payload)
+export async function insert_or_update_project(payload?: any) {
+    return request.post(BASE_URL + `console/insert_or_update_project/`, payload)
+}
+
+
+export async function insert_or_update_article(payload?: any) {
+    return request.post(BASE_URL + `console/insert_or_update_article/`, payload)
+}
+
+export async function update_article_state(payload?: any) {
+    return request.post(BASE_URL + `console/update_article_state/`, payload)
 }
 
